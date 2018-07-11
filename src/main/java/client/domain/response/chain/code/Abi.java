c2d5a402c9188fe31edb3a6e1ee35cec3ea7b63d
Bernhard Specht <bernhard@specht.net>
2018-07-11 05:41:58 +0200

    adapt get_code repsponse to fit json structure

--- src/main/java/client/domain/response/chain/code/Abi.java
+++ src/main/java/client/domain/response/chain/code/Abi.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class Abi {
 
+    private String version;
+
     private List<Type> types;
 
     private List<Struct> structs;
@@ -15,6 +17,11 @@ public class Abi {
     private List<Table> tables;
 
     private List<String> ricardianClauses;
+
+    private List<ErrorMessage> errorMessages;
+
+    // TODO: this might be of some particular type, but I couldn't find any examples of how this would look like
+    private Object abiExtensions;
 
     public Abi(){
 
@@ -61,4 +68,31 @@ public class Abi {
     public void setRicardianClauses(List<String> ricardianClauses) {
         this.ricardianClauses = ricardianClauses;
     }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<ErrorMessage> getErrorMessages() {
+        return errorMessages;
+    }
+
+    @JsonProperty("error_messages")
+    public void setErrorMessages(List<ErrorMessage> errorMessages) {
+        this.errorMessages = errorMessages;
+    }
+
+    public Object getAbiExtensions() {
+        return abiExtensions;
+    }
+
+    @JsonProperty("abi_extensions")
+    public void setAbiExtensions(List<String> abiExtensions) {
+        this.abiExtensions = abiExtensions;
+    }
+
 }
---
+++ src/main/java/client/domain/response/chain/code/ErrorMessage.java
@@ -0,0 +1,29 @@
+package client.domain.response.chain.code;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ErrorMessage {
+
+    private Integer errorCode;
+
+    private String errorMsg;
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    @JsonProperty("error_code")
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    @JsonProperty("error_msg")
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+}
