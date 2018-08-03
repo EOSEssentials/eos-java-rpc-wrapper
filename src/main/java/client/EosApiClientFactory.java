f6fbdfbf5df7bd9d93a49b06fa8d8ce55d4284ad
Bernhard Specht <bernhard@specht.net>
2018-08-03 15:22:33 +0800

    add constructor to set base urls for wallet, chain and history
    documented in readme, but not implemented in the factory

--- src/main/java/client/EosApiClientFactory.java
+++ src/main/java/client/EosApiClientFactory.java
@@ -6,18 +6,34 @@ public class EosApiClientFactory {
 
     private String baseUrl;
 
+    private String walletBaseUrl;
+
+    private String chainBaseUrl;
+
+    private String historyBaseUrl;
+
     private EosApiClientFactory(String baseUrl) {
         this.baseUrl = baseUrl;
     }
 
+    private EosApiClientFactory(String walletBaseUrl, String chainBaseUrl, String historyBaseUrl) {
+        this.walletBaseUrl = walletBaseUrl;
+        this.chainBaseUrl = chainBaseUrl;
+        this.historyBaseUrl = historyBaseUrl;
+    }
 
     public static EosApiClientFactory newInstance(String baseUrl) {
         return new EosApiClientFactory(baseUrl);
     }
 
+    public static EosApiClientFactory newInstance(String walletBaseUrl, String chainBaseUrl, String historyBaseUrl) {
+        return new EosApiClientFactory(walletBaseUrl, chainBaseUrl, historyBaseUrl);
+    }
 
     public EosApiRestClient newRestClient() {
-        return new EosApiRestClientImpl(baseUrl);
+        return baseUrl != null
+            ? new EosApiRestClientImpl(baseUrl)
+            : new EosApiRestClientImpl(walletBaseUrl, chainBaseUrl, historyBaseUrl);
     }
 
 }
