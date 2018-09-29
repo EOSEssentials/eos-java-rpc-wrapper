7edf94c0a2c8fb57da452d8e72e5d5858ddd8d79
haiwang <haiwang@bcanalyst.com>
2018-09-29 12:01:56 +0800

    update : update return in get_account method

--- src/main/java/client/domain/response/chain/account/RequiredAuth.java
+++ src/main/java/client/domain/response/chain/account/RequiredAuth.java
@@ -7,7 +7,7 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RequiredAuth {
 
-    private List<String> accounts;
+    private List<SimpleAccount> accounts;
 
     private List<Key> keys;
 
@@ -15,11 +15,11 @@ public class RequiredAuth {
 
     private List<Wait> waits;
 
-    public List<String> getAccounts() {
+    public List<SimpleAccount> getAccounts() {
         return accounts;
     }
 
-    public void setAccounts(List<String> accounts) {
+    public void setAccounts(List<SimpleAccount> accounts) {
         this.accounts = accounts;
     }
 
