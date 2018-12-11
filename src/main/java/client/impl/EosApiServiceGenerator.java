fee41b6226d29a8a855e1050d24a581e883fd97a
zhujk <zhujk@outlook.com>
2018-12-11 16:04:42 +0800

    push raw tx
    Signed-off-by: zhujk <zhujk@outlook.com>

--- src/main/java/client/impl/EosApiServiceGenerator.java
+++ src/main/java/client/impl/EosApiServiceGenerator.java
@@ -1,34 +1,44 @@
 package client.impl;
 
-import client.exception.EosApiErrorCode;
 import client.exception.EosApiError;
+import client.exception.EosApiErrorCode;
 import client.exception.EosApiException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
 public class EosApiServiceGenerator {
-
-    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-    private static Retrofit.Builder builder =
-            new Retrofit.Builder()
-                    .addConverterFactory(JacksonConverterFactory.create());
-
+    private static final Retrofit.Builder builder;
     private static Retrofit retrofit;
 
+    static {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(FAIL_ON_UNKNOWN_PROPERTIES);
+        builder = new Retrofit.Builder()
+                .client(client)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create(mapper));
+    }
+
+    private EosApiServiceGenerator() {
+    }
+
     public static <S> S createService(Class<S> serviceClass, String baseUrl) {
-
-        builder.baseUrl(baseUrl);
-        builder.client(httpClient.build());
-        builder.addConverterFactory(JacksonConverterFactory.create());
-        retrofit = builder.build();
-
+        retrofit = builder.baseUrl(baseUrl).build();
         return retrofit.create(serviceClass);
     }
 
@@ -52,7 +62,7 @@ public class EosApiServiceGenerator {
     /**
      * Extracts and converts the response error body into an object.
      */
-    private static EosApiError getEosApiError(Response<?> response) throws IOException, EosApiException {
+    private static EosApiError getEosApiError(Response<?> response) throws IOException {
         return (EosApiError) retrofit.responseBodyConverter(EosApiError.class, new Annotation[0])
                 .convert(response.errorBody());
     }
